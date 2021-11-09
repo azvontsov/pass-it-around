2dc9cf36b9b19a6df3d9637d90acff932a0aaa88
Anton <anton@Polinas-MacBook-Air.local>
2021-11-09 12:07:06 -0500

    added css style to a tag

--- server.js
+++ server.js
@@ -2,25 +2,53 @@ const express = require('express');
 
 const app = express();
 
-app.get('/', (req, res) => {
 
-    res.send(`
-    <center><h1>99 Bottles of beer on the wall</h1></center>
-    <center><a href="/98">take one down, pass it around</a></center>
-    `);
-})
 
 app.get('/:number_of_bottles', (req, res) => {
     const pageOfBottles = (req.params.number_of_bottles - 1);
+    const randomBottle = Math.floor(Math.random() * pageOfBottles);
     if (pageOfBottles < 2) {
         res.send(`
-        <center><h2>The number of bottles of beer on the wall</h2><h1>${pageOfBottles}</h1></center>
-        <center><a href="/99">take one down, start over</a></center>
+        <style>
+        .ch10 {
+            display: block;
+            width: 13ch;
+            word-break: break-all;
+            color: black;
+          }
+        </style>
+        <center><h2>The ${pageOfBottles} bottles of beer on the wall</h2></center>
+        <center><h2>The ${pageOfBottles} bottles of the beer</h2></center>
+        <center><h2><a class="ch10" href="/99">take one down, start over</a></h2></center>
+        `);
+    } else if (pageOfBottles === randomBottle) {
+        res.send(`
+        <style>
+        .ch10 {
+            display: block;
+            width: 13ch;
+            word-break: break-all;
+            color: black;
+          }
+        </style>
+        <center><h2>The ${pageOfBottles} bottles of beer on the wall</h2></center>
+        <center><h2>The ${pageOfBottles} bottles of the beer</h2></center>
+        <center><h2><a class="ch10" href="/99">take one down, start over</a></h2></center>
+        <center><h2>The ${pageOfBottles + 1} bottles of beer on the wall</h2></center>
         `);
     } else {
         res.send(`
-        <center><h2>The number of bottles of beer on the wall</h2><h1>${pageOfBottles}</h1></center>
-        <center><a href="${pageOfBottles}">take one down, pass it around</a></center>
+        <style>
+        .ch10 {
+            display: block;
+            width: 13ch;
+            word-break: break-all;
+            color: black;
+          }
+        </style>
+        <center><h2>The ${pageOfBottles} bottles of beer on the wall</h2></center>
+        <center><h2>The ${pageOfBottles} bottles of the beer</h2></center>
+        <center><h2><a a class="ch10" href="${pageOfBottles}">take one down, pass it around</a></h2></center>
         `);
     }
 })

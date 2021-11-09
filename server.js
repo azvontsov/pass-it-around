c03f2b2877bfd2d3f09451cc29aecf6ddd878979
Anton <anton@Polinas-MacBook-Air.local>
2021-11-08 23:09:21 -0500

    cenetered HTML elem

--- server.js
+++ server.js
@@ -5,22 +5,22 @@ const app = express();
 app.get('/', (req, res) => {
 
     res.send(`
-    <h1>99 Bottles of beer on the wall</h1>
-    <a href="/98">take one down, pass it around</a>
+    <center><h1>99 Bottles of beer on the wall</h1></center>
+    <center><a href="/98">take one down, pass it around</a></center>
     `);
 })
 
 app.get('/:number_of_bottles', (req, res) => {
     const pageOfBottles = (req.params.number_of_bottles - 1);
-    if (pageOfBottles < 1) {
+    if (pageOfBottles < 2) {
         res.send(`
-        <h2>The number of bottles of beer on the wall</h2><h1>${pageOfBottles}</h1>
-        <a href="/99">take one down, start over</a>
+        <center><h2>The number of bottles of beer on the wall</h2><h1>${pageOfBottles}</h1></center>
+        <center><a href="/99">take one down, start over</a></center>
         `);
     } else {
         res.send(`
-        <h2>The number of bottles of beer on the wall</h2><h1>${pageOfBottles}</h1>
-        <a href="${pageOfBottles}">take one down, pass it around</a>
+        <center><h2>The number of bottles of beer on the wall</h2><h1>${pageOfBottles}</h1></center>
+        <center><a href="${pageOfBottles}">take one down, pass it around</a></center>
         `);
     }
 })
